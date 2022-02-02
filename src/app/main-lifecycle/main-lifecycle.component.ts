c929e99b32f8d59d1b8cb16c6b6c5a05973c01a3
Pricilla de Souza Teixeira <59516715+PricillaSousaTeixeira@users.noreply.github.com>
2022-02-02 16:13:10 -0300

    Update main-lifecycle.component.ts

--- src/app/main-lifecycle/main-lifecycle.component.ts
+++ src/app/main-lifecycle/main-lifecycle.component.ts
@@ -2,25 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { Client } from '../client';
 
 @Component({
-  selector: 'app-main-lifecycle',
-  templateUrl: './main-lifecycle.component.html',
-  styleUrls: ['./main-lifecycle.component.css']
+selector: 'app-main-lifecycle',
+templateUrl: './main-lifecycle.component.html',
+styleUrls: ['./main-lifecycle.component.css']
 })
 export class MainLifecycleComponent implements OnInit {
-  foods: string[] = ["Rice", "Beans", "pizza"];
-  clients: Client[] = [];
-  name: string ;
-  age: number ;
-  food: string;
-  editClient: Client;
+foods: string[] = ["Rice", "Beans", "pizza"];
+clients: Client[] = [];
+name: string;
+age: number;
+food: string;
+editClient: Client;
 
-  constructor() { }
+constructor() { }
 
-  ngOnInit(): void {
-  }
-save(){
-  if(this.editClient==null){
-  this.clients.push({name: this.name, age: this.age, food: this.food})
+ngOnInit(): void {
+}
+save() {
+if (this.editClient == null) {
+this.clients.push({ name: this.name, age: this.age, food: this.food })
 }
 
 }
